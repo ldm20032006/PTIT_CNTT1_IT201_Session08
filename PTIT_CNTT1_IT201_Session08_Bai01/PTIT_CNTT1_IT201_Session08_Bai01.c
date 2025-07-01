de39e9f10a992bdba856424b4777881a481a9458
ldm20032006 <leduyminh2006k4@gmail.com>
2025-07-01 20:15:16 +0700

    Add files via upload

---
+++ PTIT_CNTT1_IT201_Session08_Bai01/PTIT_CNTT1_IT201_Session08_Bai01.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    int n, *arr , key;
+
+    printf("Moi ban nhap so luong phan tu: ");
+    scanf("%d", &n);
+
+    if (n <= 0 || n > 100) {
+        printf("So luong phan tu khong hop le");
+        return 0;
+    }
+    arr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        printf("Nhap phan thu thu %d : ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Moi ban nhap vao so can tim: ");
+    scanf("%d", &key);
+
+    int result = linearSearch(arr, n, key);
+    if (result == -1) {
+        printf("Khong ton tai phan tu");
+    }else {
+        printf("Vi tri thu %d ",result);
+    }
+    free(arr);
+    return 0;
+}
+// Độ phức tạp thời gian: 0(1)
